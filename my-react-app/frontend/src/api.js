68c98454db4aa9240a82bb8ed2b7ff01319b32a8
Arya-patel-22 <aryapatel22062006@gmail.com>
2026-06-04 11:16:26 +0530

    frountend and backend connected

--- my-react-app/frontend/src/api.js
+++ my-react-app/frontend/src/api.js
@@ -1,6 +1,6 @@
 // Central API configuration for the frontend
 const API_BASE_URL =
   process.env.REACT_APP_API_BASE_URL ||
-  "https://fortunate-eagerness-production-b916.up.railway.app";
+  "https://dashboard-project-production-a896.up.railway.app";
 
 export default API_BASE_URL;
